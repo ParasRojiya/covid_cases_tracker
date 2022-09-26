0dfaf5006acfefd938e23356be6d897b721f0b2a
Paras Rojiya <parasrojiya51@gmail.com>
2022-09-27 00:31:39 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Covid19 Cases Tracker App
+
+<img src="https://github.com/ParasRojiya/covid_cases_tracker/blob/master/assets/output/output.jpg" style=" height:600px; " data-target="animated-image.originalImage">
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:covid_cases_tracker/screens/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
